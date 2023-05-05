aa8a10fcdb518b0aaa1a4edcc30fe838e879f0e7
Borja García <borjagarcia.dev@gmail.com>
2023-05-05 12:03:00 +0100

    :wastebasket: Remove solution

--- src/main/java/mouse/Mouse.java
+++ src/main/java/mouse/Mouse.java
@@ -1,116 +1,34 @@
 package mouse;
 
+import mouse.MouseEventListener;
+import mouse.MouseEventType;
+import mouse.MousePointerCoordinates;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class Mouse {
     private List<MouseEventListener> listeners = new ArrayList<>();
-    public static final long clickTimeWindow = 500;
-    private long lastTimePressed;
-    private long lastTimeMoved;
-    private boolean wasButtonPressed;
-    private boolean isWaitingToTriggerSingleClick;
-    private boolean isWaitingToTriggerDoubleClick;
-    private boolean isWaitingToTriggerTripleClick;
-    private static MouseEventType eventToTrigger;
+    private final long timeWindowInMillisecondsForDoubleClick = 500;
 
     public void pressLeftButton(long currentTimeInMilliseconds) {
-        lastTimePressed = currentTimeInMilliseconds;
-        wasButtonPressed = true;
+        /*... implement this method ...*/
+    }
+
+    public void releaseLeftButton(long currentTimeInMilliseconds) {
+        /*... implement this method ...*/
     }
 
     public void move(MousePointerCoordinates from, MousePointerCoordinates to, long
             currentTimeInMilliseconds) {
-        lastTimeMoved = currentTimeInMilliseconds;
-        if (wasButtonPressed) {
-            eventToTrigger = MouseEventType.Drag;
-            notifySubscribers();
-        }
-    }
-
-    public void releaseLeftButton(long currentTimeInMilliseconds) {
-        if (wasButtonPressed) {
-            resetButtonPressedState();
-            if (wasDragging()) {
-                handleDropEvent();
-            } else {
-                handleClickEvent();
-            }
-        }
+        /*... implement this method ...*/
     }
 
     public void subscribe(MouseEventListener listener) {
         listeners.add(listener);
     }
 
-    private void resetButtonPressedState() {
-        wasButtonPressed = false;
-    }
-
-    private boolean wasDragging() {
-        return lastTimeMoved > lastTimePressed;
-    }
-
-    private void handleDropEvent() {
-        eventToTrigger = MouseEventType.Drop;
-        notifySubscribers();
-    }
-
-    private void handleClickEvent() {
-        calculateTypeOfClickEvent();
-        afterTheClickTimeWindow(() -> {
-            boolean isClick = isFinallyAclickEvent();
-            // the order of the reset state is important, must be here in the
-            // middle to make sure time window is accurate:
-            resetClickState();
-            // finally notify
-            if (isClick) {
-                notifySubscribers();
-            }
-        });
-    }
-
-    private boolean isFinallyAclickEvent() {
-        return isWaitingToTriggerSingleClick ||
-                isWaitingToTriggerDoubleClick ||
-                isWaitingToTriggerTripleClick;
-    }
-
-    private void afterTheClickTimeWindow(Runnable runnable) {
-        CompletableFuture.delayedExecutor(
-                clickTimeWindow,
-                TimeUnit.MILLISECONDS).execute(
-                runnable);
-    }
-
-    private void resetClickState() {
-        if (isWaitingToTriggerSingleClick) {
-            isWaitingToTriggerSingleClick = false;
-        }
-        if (isWaitingToTriggerDoubleClick) {
-            isWaitingToTriggerDoubleClick = false;
-        }
-        if (isWaitingToTriggerTripleClick) {
-            isWaitingToTriggerTripleClick = false;
-        }
-    }
-
-    private void calculateTypeOfClickEvent() {
-        if (!isWaitingToTriggerSingleClick) {
-            isWaitingToTriggerSingleClick = true;
-            eventToTrigger = MouseEventType.SingleClick;
-        } else if (!isWaitingToTriggerDoubleClick) {
-            isWaitingToTriggerDoubleClick = true;
-            eventToTrigger = MouseEventType.DoubleClick;
-        } else {
-            isWaitingToTriggerTripleClick = true;
-            eventToTrigger = MouseEventType.TripleClick;
-        }
-    }
-
-    private void notifySubscribers() {
-        listeners.forEach(listener -> listener.handleMouseEvent(eventToTrigger));
+    private void notifySubscribers(MouseEventType eventType) {
+        listeners.forEach(listener -> listener.handleMouseEvent(eventType));
     }
 }
